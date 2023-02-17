88926b339216422f62e32c77a2202644ffd18a4f
anand kumar <harshrajsinha1@gmail.com>
2023-02-17 22:10:39 +0530

    .

--- practice.c
+++ practice.c
@@ -2,33 +2,7 @@
 #include <stdio.h>
 int main()
 {
-    int num;
-    scanf("%d", &num);
-    if (num == 1)
-    {
-        printf("This is not prime number\n");
-    }
-    else if (num ==2)
-    {
-        printf("This is prime number\n");
-    }
-    
-    else
-    {
-        for (int i = 2; i < num; i++)
-        {
-            if (num % i == 0)
-            {
-                printf("This is not prime number\n");
-                break;
-            }
-            else
-            {
-                printf("This is prime number\n");
-                break;
-            }
-        }
-    }
+    printf("i am aanand\r cout ");
 
     return 0;
 }
