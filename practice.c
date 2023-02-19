49be554d0d6616270b5650068b34c0429ffd5b77
anand kumar <harshrajsinha1@gmail.com>
2023-02-19 20:06:51 +0530

    .

--- practice.c
+++ practice.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 int main()
 {
-    int num, k;
+    int num;
+    printf("Enter the number\n");
     scanf("%d", &num);
-    for (int i = 0; num != 0; i++)
+    if (num == 1)
     {
-        k = num % 10;
-        num = num / 10;
-        printf("%d", k);
+        printf("This is not prime number\n");
+    }
+    else if (num == 2)
+    {
+        printf("This is prime number\n");
+    }
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            printf("This is not prime number\n");
+            break;
+        }
+        else
+        {
+            printf("This is prime nubmer\n");
+            break;
+        }
     }
 
     return 0;
--- practice.cpp
+++ practice.cpp
@@ -2,14 +2,29 @@
 using namespace std;
 int main()
 {
-    int num, k;
+    int num, a = 0, b = 1, sum;
     cout << "Enter the number" << endl;
     cin >> num;
-    for (int i = 0; num != 0; i++)
+    if (num == 1)
     {
-        k = num % 10;
-        num = num / 10;
-        cout << k;
+        cout << "This is not prime number" << endl;
+    }
+    else if (num == 2)
+    {
+        cout << "This number is prime number" << endl;
+    }
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            cout << "This is not prime number" << endl;
+            break;
+        }
+        else
+        {
+            cout << "This is prime number" << endl;
+            break;
+        }
     }
 
     return 0;
